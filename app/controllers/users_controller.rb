b161e1b5ceff738abd9c53d93e4ac75c6a3b60d0
ytyeoh <ytyeoh@me.com>
2016-05-28 21:37:53 +0800

    user stage column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :identity_card)
+      params.require(:user).permit(:name, :email, :identity_card, :stage)
     end
 end
