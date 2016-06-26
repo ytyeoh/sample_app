90757604917a090d890fd9ce43762fe835f0855a
ytyeoh <yytcsl1526>
2016-06-26 16:27:19 +0800

    auto complete

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,11 +1,5 @@
-$(document).on('ready page:load', function(){
   $('#city').typeahead({
     name: "listing",
     displayKey: 'city',
     remote: "/listings/autocomplete?query=%QUERY"
   });
-
-  $('.tt-hint').addClass('form-control');
-
-
-});
