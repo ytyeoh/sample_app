c2079d6b4171a740fc24b8747054b22767241c33
ytyeoh <yytcsl1526>
2016-06-26 16:25:43 +0800

    auto complete

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,12 +1,11 @@
 $(document).on('ready page:load', function(){
-  // debugger
   $('#city').typeahead({
     name: "listing",
     displayKey: 'city',
     remote: "/listings/autocomplete?query=%QUERY"
   });
 
-  // $('.tt-hint').addClass('form-control');
+  $('.tt-hint').addClass('form-control');
 
 
 });
