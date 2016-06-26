842e30fa9a62890e84679dd3abcc5d3a030c69f2
ytyeoh <yytcsl1526>
2016-06-26 16:36:15 +0800

    autocomplete

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,7 @@
-$(document).on('ready page:load', function(){
-  $('#city').typeahead({
-    name: "listing",
-    displayKey: 'city',
-    remote: "/listings/autocomplete?query=%QUERY"
-  });
-});
+// $(document).on('ready page:load', function(){
+//   $('#city').typeahead({
+//     name: "listing",
+//     displayKey: 'city',
+//     remote: "/listings/autocomplete?query=%QUERY"
+//   });
+// });
