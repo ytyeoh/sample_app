7957783bd0b070666cdd4720d71fd10b5432813b
ytyeoh <yytcsl1526>
2016-06-26 17:21:36 +0800

    braintree login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :braintree_token, :if => :authenticate_user!
+  before_action :braintree_token
   def braintree_token
     @braintree_token = generate_client_token
   end
 
   def generate_client_token
-    if current_user.has_payment_info?
-      Braintree::ClientToken.generate(customer_id: current_user.braintree_customer_id)
-    else
-      Braintree::ClientToken.generate
+    if current_user
+      if current_user.has_payment_info?
+        Braintree::ClientToken.generate(customer_id: current_user.braintree_customer_id)
+      else
+        Braintree::ClientToken.generate
+      end
     end
   end
 end
