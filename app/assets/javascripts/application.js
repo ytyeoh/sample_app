439cf3cbee3483976a3ba0c9d9a9c6fc91c94604
ytyeoh <yytcsl1526>
2016-07-24 22:38:13 +0800

    image preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,18 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#img_prev')
+        .attr('src', e.target.result)
+        .removeClass('hidden')
+        .height(250);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
