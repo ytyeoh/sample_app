5026a8ca285d5ca16fbca9f06380d0426650dc96
ytyeoh <ytyeoh@me.com>
2016-08-03 23:34:30 +0800

    add share button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def body_class
+    "#{controller_name} #{action_name}"
+  end
+
+  def title(text)
+    content_for :title, text
+  end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text='')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
 end
