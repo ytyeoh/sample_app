d2fc54e7f7e52d041daf9b3cd7c9d7831e4cee55
ytyeoh <ytyeoh@me.com>
2016-10-15 19:44:20 +0800

    change password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
   
   private
 
