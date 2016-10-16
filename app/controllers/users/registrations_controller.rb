5f9a6763498b7bd5af5d8c9b028fe4882ed0da9b
ytyeoh <ytyeoh@me.com>
2016-10-16 20:57:34 +0800

    remove un use file

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  protected
-
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
-  
   private
 
   def update_resource(resource, params)
