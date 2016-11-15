0235d1678195976e1bfaf5f6a214142b3040bbd5
ytyeoh <ytyeoh@me.com>
2016-11-15 16:46:56 +0800

    kaminari gem add

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'paperclip-compression'
 gem 'searchkick'
 gem 'material_icons'
 gem 'dropzonejs-rails'
+gem 'kaminari'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'rails_12factor', group: :production
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -89,7 +89,7 @@ class ListingsController < ApplicationController
   end
 
   def owner
-    @listings = Listing.where(user_id: current_user.id)
+    @listings = Listing.where(user_id: current_user.id).page params[:page]
     @hash = Gmaps4rails.build_markers(@listings) do |listing, marker|
       marker.lat listing.latitude
       marker.lng listing.longitude
