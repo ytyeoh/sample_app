a982a2e5fc5884efb0db4714bbb9fe33b6f7c8da
ytyeoh <ytyeoh@me.com>
2017-01-05 16:53:09 +0800

    able hide address of listing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224071026) do
+ActiveRecord::Schema.define(version: 20170105075549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20161224071026) do
     t.integer  "furnished"
     t.integer  "area"
     t.integer  "property"
+    t.boolean  "hide"
   end
 
   add_index "listings", ["user_id"], name: "index_listings_on_user_id", using: :btree
